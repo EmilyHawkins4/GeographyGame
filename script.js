9138c4cef4cf45abdb9f75fad4122ee12fe956b6
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-10-25 07:03:58 -0700

    i dont even know

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var countriesEntered = [];
 
 // array of real countries
 var countryArray = ["canada", "united states", "mexico", "guatemala", "belize", "el salvador", "honduras", "nicaragua", "costa rica", "panama", "bahamas", "cuba", "haiti", "dominican republic", "jamaica", "st lucia", "st kitts and nevis", "st vincent", "dominica", "grenada", "trinidad and tobago", "barbados", "antigua and barbuda", "colombia", "venezuela", "argentina", "ecuador", "brazil", "chile", "guyana", "suriname", "peru", "bolivia", "uruguay", "paraguay", "iceland", "norway", "finland", "sweden", "denmark", "czechia", "romania", "ukraine", "belarus", "bulgaria", "moldova", "croatia", "north macedonia", "bosnia and herzegovina", "luxembourg", "liechtenstein", "albania", "serbia", "kosovo", "san marino", "andorra", "united kingdom", "estonia", "latvia", "lithuania", "spain", "france", "portugal", "netherlands", "belgium", "switzerland", "hungary", "germany", "austria", "poland", "turkey", "vatican", "italy", "montenegro", "monaco", "greece", "malta", "cyprus", "slovakia", "ireland", "slovenia", "timor leste", "malaysia", "indonesia", "brunei", "singapore", "philippines", "taiwan", "china", "russia", "mongolia", "bhutan", "india", "sri lanka", "bangladesh", "pakistan", "afghanistan", "uzbekistan", "tajikistan", "turkmenistan", "kyrgyzstan", "kazakhstan", "azerbaijan", "armenia", "georgia", "jordan", "syria", "kuwait", "qatar", "united arab emirates", "yemen", "oman", "saudi arabia", "iraq", "nepal", "iran", "israel", "maldives", "lebanon", "palestine", "north korea", "south korea", "japan", "vietnam", "bahrain", "thailand", "cambodia", "myanmar", "laos", "djibouti", "eritrea", "egypt", "chad", "sudan", "south sudan", "mozambique", "malawi", "somalia", "senegal", "ghana", "guinea", "guinea bissau", "burkina faso", "democratic republic of the congo", "republic of the congo", "central african republic", "uganda", "rwanda", "burundi", "tanzania", "kenya", "zambia", "zimbabwe", "south africa", "eswatini", "lesotho", "namibia", "botswana", "ivory coast", "angola", "seychelles", "sierra leone", "morocco", "algeria", "libya", "tunisia", "sao tome and principe", "mali", "nigeria", "niger", "mauritania", "comoros", "madagascar", "gambia", "liberia", "equatorial guinea", "cape verde", "mauritius", "ethiopia", "gabon", "cameroon", "benin", "togo", "nauru", "palau", "new zealand", "australia", "solomon islands", "marshall islands", "papua new guinea", "vanuatu", "micronesia", "fiji", "samoa", "tuvalu", "kiribati", "tonga"]
+console.log(countryArray[76]);
+
 console.log(countryArray.length);
 function initMap() {
 
@@ -62,6 +64,14 @@ function initMap() {
   southAmerica.innerHTML = "<strong>South America:</strong>  " + saNum + "/12";
   document.getElementById("content").appendChild(southAmerica)
 
+  for(i=0; i<countryArray.length; i++){
+    map.data.loadGeoJson('countries/'+countryArray[i]+'.geojson');
+    map.data.setStyle({
+      fillColor: "#fc0352",
+      strokeColor: "#fc0352"
+    });
+  }
+
 }
 
 function checkInput(){
@@ -72,11 +82,7 @@ function checkInput(){
     for(i=0; i<countryArray.length; i++){
       if(userstuff==countryArray[i]){
         document.getElementById("stuff").value = null;
-        map.data.loadGeoJson('countries/'+userstuff+'.geojson');
-        map.data.setStyle({
-          fillColor: "#fc0352",
-          strokeColor: "#fc0352"
-        });
+        map.data.remove('countries/'+userstuff+'.geojson');
         console.log("countries/"+userstuff+".geojson");
         totalNum++;
       }
@@ -92,7 +98,7 @@ PROBLEMS I SHOULD BE FIXING
       - Nigeria
       - Guniea Bissau
   - make counter work <<<< PRIORITY 4
-  - make geo jsons dissapear not reveal
+  - make geo jsons dissapear not reveal <<<< PRIORITY 7
   - start button to reveal input field <<<< PRIORITY 2
   - shouldn't input same country twice <<<< PRIORITY 5
   - add a timer <<<< PRIORITY 3
