7d05703884cf5c1a902e88f3c2e9209001c144e4
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-11-10 08:31:33 -0800

    stuff

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ function initMap() {
   // make map
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 20, lng: 0 },
-    zoom: 2,
+    zoom: 2.5,
   });
+  console.log(CountryMap.get(andorra));
 
   // no labels
   var customStyled = [
