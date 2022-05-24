e01880560cc4e3dcfb24a4d8c18891277f2cb6ae
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2022-05-23 23:04:34 -0700

    time

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ let map;
 
 
 // all countries
-var countryArray = ["canada", "united states of america", "mexico", "guatemala", "belize", "el salvador", "honduras", "nicaragua", "costa rica", "panama", "bahamas", "cuba", "haiti", "dominican republic", "jamaica", "saint lucia", "saint kitts and nevis", "saint vincent and the grenadines", "dominica", "grenada", "trinidad and tobago", "barbados", "antigua and barbuda", "colombia", "venezuela", "argentina", "ecuador", "brazil", "chile", "guyana", "suriname", "peru", "bolivia", "uruguay", "paraguay", "iceland", "norway", "finland", "sweden", "denmark", "czechia", "romania", "ukraine", "belarus", "bulgaria", "moldova", "croatia", "north macedonia", "bosnia and herzegovina", "luxembourg", "liechtenstein", "albania", "serbia", "kosovo", "san marino", "andorra", "united kingdom", "estonia", "latvia", "lithuania", "spain", "france", "portugal", "netherlands", "belgium", "switzerland", "hungary", "germany", "austria", "poland", "turkey", "vatican", "italy", "montenegro", "monaco", "greece", "malta", "cyprus", "slovakia", "ireland", "slovenia", "timor leste", "malaysia", "indonesia", "brunei", "singapore", "philippines", "taiwan", "china", "russia", "mongolia", "bhutan", "india", "sri lanka", "bangladesh", "pakistan", "afghanistan", "uzbekistan", "tajikistan", "turkmenistan", "kyrgyzstan", "kazakhstan", "azerbaijan", "armenia", "georgia", "jordan", "syria", "kuwait", "qatar", "united arab emirates", "yemen", "oman", "saudi arabia", "iraq", "nepal", "iran", "israel", "maldives", "lebanon", "palestine", "north korea", "south korea", "japan", "vietnam", "bahrain", "thailand", "cambodia", "myanmar", "laos", "djibouti", "eritrea", "egypt", "chad", "sudan", "south sudan", "mozambique", "malawi", "somalia", "senegal", "ghana", "guinea", "guinea bissau", "burkina faso", "democratic republic of the congo", "republic of congo", "central african republic", "uganda", "rwanda", "burundi", "tanzania", "kenya", "zambia", "zimbabwe", "south africa", "eswatini", "lesotho", "namibia", "botswana", "ivory coast", "angola", "seychelles", "sierra leone", "morocco", "algeria", "libya", "tunisia", "sao tome and principe", "mali", "nigeria", "niger", "mauritania", "comoros", "madagascar", "gambia", "liberia", "equatorial guinea", "cape verde", "mauritius", "ethiopia", "gabon", "cameroon", "benin", "togo", "nauru", "palau", "new zealand", "australia", "solomon islands", "marshall islands", "papua new guinea", "vanuatu", "micronesia", "fiji", "samoa", "tuvalu", "kiribati", "tonga"]
+let countryArray = ["canada", "united states of america", "mexico", "guatemala", "belize", "el salvador", "honduras", "nicaragua", "costa rica", "panama", "bahamas", "cuba", "haiti", "dominican republic", "jamaica", "saint lucia", "saint kitts and nevis", "saint vincent and the grenadines", "dominica", "grenada", "trinidad and tobago", "barbados", "antigua and barbuda", "colombia", "venezuela", "argentina", "ecuador", "brazil", "chile", "guyana", "suriname", "peru", "bolivia", "uruguay", "paraguay", "iceland", "norway", "finland", "sweden", "denmark", "czechia", "romania", "ukraine", "belarus", "bulgaria", "moldova", "croatia", "north macedonia", "bosnia and herzegovina", "luxembourg", "liechtenstein", "albania", "serbia", "kosovo", "san marino", "andorra", "united kingdom", "estonia", "latvia", "lithuania", "spain", "france", "portugal", "netherlands", "belgium", "switzerland", "hungary", "germany", "austria", "poland", "turkey", "vatican", "italy", "montenegro", "monaco", "greece", "malta", "cyprus", "slovakia", "ireland", "slovenia", "timor leste", "malaysia", "indonesia", "brunei", "singapore", "philippines", "taiwan", "china", "russia", "mongolia", "bhutan", "india", "sri lanka", "bangladesh", "pakistan", "afghanistan", "uzbekistan", "tajikistan", "turkmenistan", "kyrgyzstan", "kazakhstan", "azerbaijan", "armenia", "georgia", "jordan", "syria", "kuwait", "qatar", "united arab emirates", "yemen", "oman", "saudi arabia", "iraq", "nepal", "iran", "israel", "maldives", "lebanon", "palestine", "north korea", "south korea", "japan", "vietnam", "bahrain", "thailand", "cambodia", "myanmar", "laos", "djibouti", "eritrea", "egypt", "chad", "sudan", "south sudan", "mozambique", "malawi", "somalia", "senegal", "ghana", "guinea", "guinea bissau", "burkina faso", "democratic republic of the congo", "republic of congo", "central african republic", "uganda", "rwanda", "burundi", "tanzania", "kenya", "zambia", "zimbabwe", "south africa", "eswatini", "lesotho", "namibia", "botswana", "ivory coast", "angola", "seychelles", "sierra leone", "morocco", "algeria", "libya", "tunisia", "sao tome and principe", "mali", "nigeria", "niger", "mauritania", "comoros", "madagascar", "gambia", "liberia", "equatorial guinea", "cape verde", "mauritius", "ethiopia", "gabon", "cameroon", "benin", "togo", "nauru", "palau", "new zealand", "australia", "solomon islands", "marshall islands", "papua new guinea", "vanuatu", "micronesia", "fiji", "samoa", "tuvalu", "kiribati", "tonga"]
 
 // hashmap of country/continent relationshps
 let countryMap = new Map([["canada", "N"], ["united states of america", "N"], ["mexico", "N"], ["guatemala", "N"], ["belize", "N"], ["el salvador", "N"], ["honduras", "N"], ["nicaragua", "N"], ["costa rica", "N"], ["panama", "N"], ["bahamas", "N"], ["cuba", "N"], ["haiti", "N"], ["dominican republic", "N"], ["jamaica", "N"], ["saint lucia", "N"], ["saint kitts and nevis", "N"], ["saint vincent and the grenadines", "N"], ["dominica", "N"], ["grenada", "N"], ["trinidad and tobago", "N"], ["barbados", "N"], ["antigua and barbuda", "N"], ["colombia", "S"], ["venezuela", "S"], ["argentina", "S"], ["ecuador", "S"], ["brazil", "S"], ["chile", "S"], ["guyana", "S"], ["suriname", "S"], ["peru", "S"], ["bolivia", "S"], ["uruguay", "S"], ["paraguay", "S"], ["iceland", "E"], ["norway", "E"], ["sweden", "E"], ["finland", "E"], ["denmark", "E"], ["czechia", "E"], ["romania", "E"], ["ukraine", "E"], ["belarus", "E"], ["bulgaria", "E"], ["moldova", "E"], ["croatia", "E"], ["north macedonia", "E"],["bosnia and herzegovina", "E"], ["luxembourg", "E"], ["liechtenstein", "E"], ["albania", "E"], ["serbia", "E"], ["kosovo", "E"], ["san marino", "E"], ["andorra", "E"], ["united kingdom", "E"], ["estonia", "E"], ["latvia", "E"], ["lithuania", "E"], ["spain", "E"], ["france", "E"], ["portugal", "E"], ["netherlands", "E"], ["belgium", "E"], ["switzerland", "E"], ["hungary", "E"], ["germany", "E"], ["austria", "E"], ["poland", "E"], ["turkey", "E"], ["vatican", "E"], ["italy", "E"], ["montenegro", "E"], ["monaco", "E"], ["greece", "E"], ["malta", "E"], ["cyprus", "E"], ["slovakia", "E"], ["slovenia", "E"], ["ireland", "E"], ["timor leste", "A"], ["malaysia", "A"], ["indonesia", "A"], ["brunei", "A"], ["singapore", "A"], ["philippines", "A"], ["taiwan", "A"], ["china", "A"], ["russia", "A"], ["mongolia", "A"], ["bhutan", "A"], ["india", "A"], ["sri lanka", "A"], ["bangladesh", "A"], ["pakistan", "A"], ["afghanistan", "A"], ["uzbekistan", "A"], ["tajikistan", "A"], ["turkmenistan", "A"], ["kyrgyzstan", "A"], ["kazakhstan", "A"], ["azerbaijan", "A"], ["armenia", "A"], ["georgia", "A"], ["jordan", "A"], ["syria", "A"], ["kuwait", "A"], ["qatar", "A"], ["united arab emirates", "A"], ["yemen", "A"], ["oman", "A"], ["saudi arabia", "A"], ["iraq", "A"], ["nepal", "A"], ["iran", "A"], ["israel", "A"], ["maldives", "A"], ["lebanon", "A"], ["palestine", "A"], ["north korea", "A"], ["south korea", "A"], ["japan", "A"], ["vietnam", "A"], ["bahrain", "A"], ["thailand", "A"], ["cambodia", "A"], ["myanmar", "A"], ["laos", "A"], ["djibouti", "F"], ["eritrea", "F"], ["egypt", "F"], ["chad", "F"], ["sudan", "F"], ["south sudan", "F"], ["mozambique", "F"], ["malawi", "F"], ["somalia", "F"], ["senegal", "F"], ["ghana", "F"], ["guinea", "F"], ["guinea bissau", "F"], ["burkina faso", "F"], ["democratic republic of the congo", "F"], ["republic of congo", "F"], ["central african republic", "F"], ["uganda", "F"], ["rwanda", "F"], ["burundi", "F"], ["tanzania", "F"], ["kenya", "F"], ["zambia", "F"], ["zimbabwe", "F"], ["south africa", "F"], ["eswatini", "F"], ["lesotho", "F"], ["namibia", "F"], ["botswana", "F"], ["ivory coast", "F"], ["angola", "F"], ["seychelles", "F"], ["sierra leone", "F"], ["morocco", "F"], ["algeria", "F"], ["libya", "F"], ["tunisia", "F"], ["sao tome and principe", "F"], ["mali", "F"], ["nigeria", "F"], ["niger", "F"], ["mauritania", "F"], ["comoros", "F"], ["madagascar", "F"], ["gambia", "F"], ["liberia", "F"], ["equatorial guinea", "F"], ["cape verde", "F"], ["mauritius", "F"], ["ethiopia", "F"], ["gabon", "F"], ["cameroon", "F"], ["benin", "F"], ["togo", "F"], ["nauru", "O"], ["palau", "O"], ["new zealand", "O"], ["australia", "O"], ["solomon islands", "O"], ["marshall islands", "O"], ["papua new guinea", "O"], ["vanuatu", "O"], ["micronesia", "O"], ["fiji", "O"], ["samoa", "O"], ["tuvalu", "O"], ["kiribati", "O"], ["tonga", "O"]]);
 
 
 // number of countries entered
-var totalNum=0;
+let totalNum=0;
 
 // executes every time page reloads
 function initMap() {
 
-  var infowindow = new google.maps.InfoWindow();
+  let infowindow = new google.maps.InfoWindow();
 
   // make map
   map = new google.maps.Map(document.getElementById("map"), {
@@ -23,7 +23,7 @@ function initMap() {
   });
 
   // no labels (country names) on the map
-  var customStyled = [
+  let customStyled = [
     {
     featureType: "all",
     elementType: "labels",
@@ -34,8 +34,8 @@ function initMap() {
 
     // show name on hover
     map.data.addListener('mouseover', function(event) {
-      var feat = event.feature;
-      var html = "<b>"+feat.getProperty('ADMIN')+"</b>";
+      let feat = event.feature;
+      let html = "<b>"+feat.getProperty('ADMIN')+"</b>";
       infowindow.setContent(html);
       infowindow.setPosition(event.latLng);
       infowindow.setOptions({pixelOffset: new google.maps.Size(0,-34)});
@@ -48,20 +48,20 @@ function initMap() {
 function startGame() {
   
   // display input box
-  var inputDiv = document.createElement("div");
+  let inputDiv = document.createElement("div");
   inputDiv.innerHTML="<input type='text' id='stuff' spellcheck='true' oninput='checkInput()' >"
   inputDiv.id="inputDiv"
   document.getElementById("inputBox").appendChild(inputDiv);
 
   // display give up button
-  var giveup = document.createElement("div");
+  let giveup = document.createElement("div");
   giveup.id = "giveUp";
-  var giveupdiv = document.createElement("div")
+  let giveupdiv = document.createElement("div")
   giveupdiv.id = "buttonGiveup";
   giveup.appendChild(giveupdiv);
   document.getElementById("map").appendChild(giveup);
-  var giveUpButton = document.createElement("button");
-  var buttonText = document.createElement("span");
+  let giveUpButton = document.createElement("button");
+  let buttonText = document.createElement("span");
   giveUpButton.innerHTML= "Give Up!"
   giveUpButton.appendChild(buttonText);
   giveUpButton.onclick = giveUp;
@@ -69,7 +69,7 @@ function startGame() {
   document.getElementById("buttonGiveup").appendChild(giveUpButton);
   
   // start timer
-  var second = 0;
+  let second = 0;
   function timer (val) { 
     return val > 9 ? val : "0" + val; 
   }
@@ -108,7 +108,6 @@ function checkInput(){
 
       // display win page
       if(totalNum==197){
-        alert("you win!");
         win();
       }
 
@@ -121,8 +120,8 @@ function giveUp(){
 
     // countries/mins/seconds
     let numCountries = 197-countryArray.length;
-    let mins = "x"; //document.getElementById("minutes").value; 
-    let secs = "x"; //document.getElementById("seconds").value;
+    let mins = document.getElementById("minutes").innerText;
+    let secs = document.getElementById("seconds").innerText;
 
   // remove input section
   if(document.getElementById("content")){
@@ -155,7 +154,7 @@ function giveUp(){
 
   // change geojson colors & add names back
   map.data.setStyle(function(feature) {
-    var color = "green";
+    let color = "green";
     if (countryArray.includes(feature.j.ADMIN.toLowerCase())) {
         color = 'red';
     }
@@ -167,10 +166,10 @@ function giveUp(){
 });
   
   for(let i=0; i<countryArray.length; i++){
-    var country = countryArray[i];
+    let country = countryArray[i];
     map.data.loadGeoJson('countries/'+country+'.geojson');
   }
-  var customStyled = [
+  let customStyled = [
     {
     featureType: "all",
     elementType: "labels",
@@ -183,8 +182,8 @@ function giveUp(){
 // called when user wins
 function win(){
 
-  let mins = "x"; //document.getElementById("minutes").value; 
-  let secs = "x"; //document.getElementById("seconds").value;
+  let mins = document.getElementById("minutes").innerText;
+  let secs = document.getElementById("seconds").innerText;
 
   // remove input section
   if(document.getElementById("content")){
@@ -213,7 +212,7 @@ function win(){
   document.getElementById("winlose").appendChild(content);
 
   // add country names back
-  var customStyled = [
+  let customStyled = [
     {
     featureType: "all",
     elementType: "labels",
@@ -244,7 +243,7 @@ function win(){
 
 // updates the country counts based on continent
 function updateCountryCount(country){
-    var continent = countryMap.get(country);
+    let continent = countryMap.get(country);
 
     switch(continent){
       case "S":
@@ -332,8 +331,3 @@ function spellCheck(word){
   }
   return word;
 }
-
-// problems & improvements: 
-  // [ ] take away give up button
-  // [ ] stop timer
-  // [ ] store time
